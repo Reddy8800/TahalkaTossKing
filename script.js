5cbf9aa41de502899276622f3b96c265575d59a2
codearvind07 <arvindfb07@gmail.com>
2025-12-11 18:35:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// ---- UPDATE THESE VALUES ---- //
+
+document.getElementById("channelLogo").src = "logo.jpg";  
+document.getElementById("channelName").innerText = "𝗝𝗮𝗻𝘃𝗶 𝗥𝗮𝗻𝗲 2018 (Official)"; 
+document.getElementById("subscriberCount").innerText = "49019+";
+
+// Want auto-fetch subscribers?  
+// Tell me your channel username: @yourchannelname
+
+
+// ---- AUTO REDIRECT AFTER 5 SECONDS ---- //
+setTimeout(function () {
+    window.location.href = "https://t.me/+etU4wyyoJ1A0NGJl"; 
+}, 20000);
